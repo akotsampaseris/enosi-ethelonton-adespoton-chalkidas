c050b721a177262d088c310bb5e74ef798d80950
akotsampaseris <a.kotsampaseris@gmail.com>
2026-02-25 14:58:09 +0200

    chore: add draft preview support for pre-prod envs

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,12 @@
 import { createClient } from "next-sanity";
 
+const isDraft = process.env.NODE_ENV === "development" || process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
+
 export const client = createClient({
-  projectId: "w983lvut",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+    apiVersion: "2024-01-01",
+    useCdn: !isDraft, // Don't use CDN for drafts
+    perspective: isDraft ? "previewDrafts" : "published",
+    token: isDraft ? process.env.SANITY_API_TOKEN : undefined,
 });

dafcd45f7fdb9b27470e06c877530f112ef9fc31
akotsampaseris <a.kotsampaseris@gmail.com>
2026-04-05 19:34:50 +0300

    chore: restructure pages under locale routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,31 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { NextIntlClientProvider } from "next-intl";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Ένωση Εθελοντών Αδέσποτων Χαλκίδας",
-  description: "Ένωση Εθελοντών Αδέσποτων Χαλκίδας",
+    title: "Ένωση Εθελοντών Αδέσποτων Χαλκίδας",
+    description: "Ένωση Εθελοντών Αδέσποτων Χαλκίδας",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+export default async function RootLayout({ children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
+    const { locale } = await params;
+
+    return (
+        <html lang={locale}>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <NextIntlClientProvider>{children}</NextIntlClientProvider>
+            </body>
+        </html>
+    );
 }
